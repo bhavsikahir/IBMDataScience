58126248fbfb0cc5fca12b55b522cc482e345d60
bhavsikahir <120907443+bhavsikahir@users.noreply.github.com>
2023-09-12 12:34:08 +0530

    Adding python file for assesment
    Course : Tools for Data Science

---
+++ DataScinceEcosystem.py
@@ -0,0 +1,80 @@
+# %% [markdown]
+# <h1>Data Science Tools and Ecosystem</h1>
+# 
+
+# %% [markdown]
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+# <b>Objectives:</b>
+# 
+# List popular languages that Data Scientists use.<br>
+# List commonly used libraries used by Data Scientists.<br>
+# List of Data Science tools.<br>
+
+# %% [markdown]
+# Some of the popular languages that Data Scientists use are:
+# <ol>
+# <li>Python</li>
+# <li>R</li>
+# <li>SQL</li>
+# <li>Java</li>
+# <li>Julia</li>
+# <li>Scala</li>
+# <li>C/C++</li>
+# <li>JavaScript</li>
+# </ol>
+
+# %% [markdown]
+# Some of the commonly used libraries used by Data Scientists include:
+# <ol>
+# <li>TensorFlow</li>
+# <li>NumPy</li>
+# <li>SciPy</li>
+# <li>Pandas</li>
+# <li>Matplotlib</li>
+# <li>Keras</li>
+# <li>SciKit-Learn</li>
+# <li>PyTorch</li>
+# <li>Scrapy</li>
+# <li>BeautifulSoup</li>
+# <li>LightGBM</li>
+# <li>ELI5</li>
+# <li>Theano</li>
+# <li>NuPIC</li>
+# <li>Ramp</li>
+# <li>Pipenv</li>
+# <li>Bob</li>
+# <li>PyBrain</li>
+# <li>Caffe2</li>
+# <li>Chainer</li>
+# </ol>
+
+# %% [markdown]
+# Data Science tools:
+# 
+# <table>
+# <tr><th>Data Science Tools</th></tr>
+# <tr><td>Jupyter</td></tr>
+# <tr><td>RStudio</td></tr>
+# <tr><td>Apache Spark</td></tr>
+# </table>
+
+# %% [markdown]
+# <h3>Below are a few examples of evaluating arithmetic expressions in Python</h3>
+
+# %%
+(3*4)+5
+# This a simple arithmetic expression to mutiply then add integers
+
+# %%
+200/60
+# This will convert 200 minutes to hours by diving by 60
+
+# %% [markdown]
+# <h2><b>Author</b></h2>
+# Bhavsik
+
+# %%
+
+
+
